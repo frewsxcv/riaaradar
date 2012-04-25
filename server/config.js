5aa8184ea2ab4208ce8acfceb285dce6e5e2c54c
Corey Farwell <coreyf@rwell.org>
2012-04-24 20:22:45 -0700

    configuration file for server

---
+++ server/config.js
@@ -0,0 +1,9 @@
+exports.pgSettings = {
+    user: "musicbrainz",
+    password: "musicbrainz",
+    database: "musicbrainz_db",
+    host: "localhost",
+    port: 5432
+};
+
+exports.httpPort = 4567;
