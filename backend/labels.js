26e3103ea444e3e72424a5e3aec91c91eaee9518
Corey Farwell <coreyf@rwell.org>
2012-07-05 10:45:27 -0700

    alphabetize labels

--- backend/labels.js
+++ backend/labels.js
@@ -12,28 +12,32 @@ exports.labels = {
         name: 'Beggars Banquet',
         source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=b'
     },
-    '19d052fa-570a-4b17-9a3d-8f2f029b7b57': {
-        name: 'Universal Music Group',
-        source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=u'
-    },
-    '05dac601-c683-4c02-afc4-4a03cd01c25f': {
-        name: 'PIAS Recordings',
-        source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=p'
-    },
     'abea2d3e-eabf-4480-ab24-9382dd642c73': {
         name: 'Capitol Records',
         source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=c'
     },
-    '38dc88de-7720-4100-9d5b-3cdc41b0c474': {
-        name: 'Sub Pop Records',
-        source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=s'
+    '8f638ddb-131a-4cc3-b3d4-7ebdac201b55': {
+        name: 'Epic Records',
+        source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=e'
     },
     '4d204722-f022-4b6e-8b76-0e06a45ffcdc': {
         name: 'Hydra Head Records',
         source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=h'
     },
-    '8f638ddb-131a-4cc3-b3d4-7ebdac201b55': {
-        name: 'Epic Records',
-        source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=e'
+    '05dac601-c683-4c02-afc4-4a03cd01c25f': {
+        name: 'PIAS Recordings',
+        source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=p'
+    },
+    'dd6afc9a-6352-4c96-aeb6-9a73e2bf1519': {
+        name: 'Secretly Canadian',
+        source: 'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=s'
+    },
+    '38dc88de-7720-4100-9d5b-3cdc41b0c474': {
+        name: 'Sub Pop Records',
+        source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=s'
+    },
+    '19d052fa-570a-4b17-9a3d-8f2f029b7b57': {
+        name: 'Universal Music Group',
+        source: 'http://www.riaa.com/aboutus.php?content_selector=aboutus_members&f=u'
     }
 };
