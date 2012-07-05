67d35176fa815563e7219ac917cdd63468b54cb8
Corey Farwell <coreyf@rwell.org>
2012-07-05 10:45:39 -0700

    changed tab spacing

--- backend/query.sql
+++ backend/query.sql
@@ -11,29 +11,24 @@ WITH RECURSIVE get_labels(prev_rel, prev_label, label) AS
       AND release.release_group = release_group.id
       AND release_group.gid = '%s'
   UNION
-  SELECT link_type.name,
-    CASE 
-      WHEN link_type.name = 'label rename'
-        THEN l_label_label.entity0
-      ELSE
-        l_label_label.entity1
-    END,
-    CASE 
-      WHEN link_type.name = 'label rename'
-        THEN l_label_label.entity1
-      ELSE
-        l_label_label.entity0
-    END
-    FROM l_label_label, link, get_labels, link_type
-    WHERE l_label_label.link = link.id
+   SELECT link_type.name,
+      CASE WHEN link_type.name = 'label rename'
+         THEN l_label_label.entity0
+         ELSE l_label_label.entity1
+      END,
+      CASE WHEN link_type.name = 'label rename'
+         THEN l_label_label.entity1
+         ELSE l_label_label.entity0
+      END
+   FROM l_label_label, link, get_labels, link_type
+   WHERE l_label_label.link = link.id
       AND link.link_type = link_type.id
       AND ((link_type.name = 'label ownership'
-          OR link_type.name = 'label reissue'
-          OR link_type.name = 'label distribution')
-          AND l_label_label.entity1 = get_labels.label
-        OR
-          link_type.name = 'label rename'
-          AND l_label_label.entity0 = get_labels.label))
+            OR link_type.name = 'label reissue'
+            OR link_type.name = 'label distribution')
+         AND l_label_label.entity1 = get_labels.label
+         OR link_type.name = 'label rename'
+            AND l_label_label.entity0 = get_labels.label))
 
 SELECT DISTINCT label.gid AS gid, label_name.name AS name, get_labels.prev_rel AS prev_rel,
   CASE
