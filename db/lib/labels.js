89312f38494c9f3c27cf928e14688365d9307933
Corey Farwell <coreyf@rwell.org>
2012-07-11 20:14:11 -0700

    commiting all the necessary libraries for the backend

---
+++ db/lib/labels.js
@@ -0,0 +1,153 @@
+/* RIAA affiliated labels and their MusicBrainz ID 
+ * Here's how each entry should look:
+ * 
+ * '<musicbrainz id of label>':
+ *     source: '<url of site you found proving label is an RIAA member>',
+*/
+
+{
+    // !K7
+    '893364ff-1830-4694-833d-93751f14f984':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=#',
+
+    // 112 records not in musicbrainz
+
+    // 1500 Records
+    '15bc8c1a-6cf5-4e9f-8218-ecc0894b053a':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=#',
+
+
+    // 19 Recordings
+    'dc435fb5-3c06-4e97-b1f6-f814f37703a3':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=#',
+
+    // 2-4-1 records not in musicbrainz
+
+    // 2012 sdtk not in musicbrainz
+
+    // 21 Records
+    'bf2dbd91-7d1f-4eee-9798-15ef44fdf596':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=#',
+
+    // 2PAC INT UNREL MAST not in musicbrainz
+
+    // can't confirm 333 Music - I think they're not in musicbrainz
+
+    // can't confirm 4 Life
+
+    // 4AD
+    'a539bb1e-f2e1-4b45-9db8-8053841e7503':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=#',
+
+    // 4Troops Cat seems to be band-specific? may be under Sony Masterworks
+
+    // 50 Cent Movie is almost certainly artist-specific, probably under something else
+
+    // 500 YEARS LATER not in musicbrainz, but 300 years later is. RIAA typo?
+
+     // 604 Records
+    '03fe64fe-8260-456a-a26e-fcb3ac79423f':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=#',
+
+    // 7-10 Music not in musicbrainz
+
+    // 7EVENTY 5IVE not in musicbrainz
+
+    // 8TH IMPRESSION MUSIC not in musicbrainz
+
+    // 98 POUNDER RECORDS not in musicbrainz
+
+    // about 1/3 of page # done above */
+
+    // Alternative Distribution Alliance
+    '43defeac-9c81-4efa-b1db-a99e6fe40c9b':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=a',
+
+    // Atlantic
+    '50c384a2-0b44-401b-b893-8181173339c7':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=a',
+
+    // Beggars Banquet
+    '5917da67-b605-4a08-a5a0-a6874501dda4':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=b',
+
+    // Capitol Records
+    'abea2d3e-eabf-4480-ab24-9382dd642c73':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=c',
+
+    // Columbia Records
+    '011d1192-6f65-45bd-85c4-0400dd45693e':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=c',
+
+    // Decca
+    'f18f3b31-8263-4de3-966a-fda317492d3d':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=d',
+
+    // EMI Classics
+    'f3b662ac-d029-4c87-a9bd-2f2f65b21e4f':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=e',
+
+    // Epic Records
+    '8f638ddb-131a-4cc3-b3d4-7ebdac201b55':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=e',
+
+    // Hydra Head Records
+    '4d204722-f022-4b6e-8b76-0e06a45ffcdc':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=h',
+
+    // InsideOut Music
+    'af64e67e-e3e9-4bc3-bb1a-f8c23f285b31':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=i',
+
+    // Interscope
+    '2182a316-c4bd-4605-936a-5e2fac52bdd2':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=i',
+
+    // Island Records
+    'dfd92cd3-4888-46d2-b968-328b1feb2642':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=i',
+
+    // MCA Records
+    '46a3941a-c810-47a1-974f-955effec4d09':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=m',
+
+    // Mute
+    'e0b106a5-4add-4839-9e40-c192457e1bf8':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=m',
+
+    // Parlophone
+    'df7d1c7f-ef95-425f-8eef-445b3d7bcbd9':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=p',
+
+    // PIAS Recordings
+    '05dac601-c683-4c02-afc4-4a03cd01c25f':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=p',
+
+    // Secretly Canadian
+    'dd6afc9a-6352-4c96-aeb6-9a73e2bf1519':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=s',
+
+    // Sony Music Entertainment (Japan)
+    '0331bcea-3905-4893-9874-1d3f10480625':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=s',
+
+    // Sub Pop Records
+    '38dc88de-7720-4100-9d5b-3cdc41b0c474':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=s',
+
+    // Universal Music Group
+    '19d052fa-570a-4b17-9a3d-8f2f029b7b57':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=u',
+
+    // Warner Bros. Records
+    'c595c289-47ce-4fba-b999-b87503e8cb71':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=w',
+
+    // Word Entertainment
+    '3d62ec4a-2af0-403f-b58c-1df90a1ac7ae':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=w',
+
+    // XL Recordings
+    '14221f01-8939-4ea0-b8f1-b5a21beae80a':
+        'http://riaa.com/aboutus.php?content_selector=aboutus_members&f=x'
+}
