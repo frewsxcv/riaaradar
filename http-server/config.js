f2c4aebd51a06145e7f2c4cd8a53eb43a76cd282
Corey Farwell <coreyf@rwell.org>
2012-07-17 18:21:02 -0700

    config file no longer needed

--- http-server/config.js
+++
@@ -1,9 +0,0 @@
-exports.pgSettings = {
-    user: "musicbrainz",
-    password: "musicbrainz",
-    database: "musicbrainz_db",
-    host: "localhost",
-    port: 5432
-};
-
-exports.httpPort = 4567;
