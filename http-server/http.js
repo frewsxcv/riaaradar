e0201482844e9a8166447c70d4e9d98aebd722cd
Corey Farwell <coreyf@rwell.org>
2012-07-22 18:17:22 -0700

    refactored and now reflects new riaaPath.js libary

--- http-server/http.js
+++ http-server/http.js
@@ -3,43 +3,41 @@
 // Imports
 var http = require("http");
 var urlParse = require("url").parse;
+var riaaTree = require("../riaaTree/dist/riaaTree.js").riaaTree;
 
 // HTTP server port
 var httpPort = 4567;
 
-// Respond to an invalid request
-var respondToInvalidReq = function (res) {
-    res.writeHead(400, {
-        'Content-Type': 'application/json'
-    });
-    res.end(JSON.stringify({
-        error: "mbid not specified"
-    }), "ascii");
+// Respond to a valid request
+var generateBody = function (mbid, res) {
+    var nodes = [],
+        curr = riaaTree[mbid];
+    while (curr !== undefined) {
+        nodes.push({
+            "name": curr.name,
+            "parentRel": curr.parentRel || undefined,
+            "mbid": mbid
+        });
+        mbid = curr.parentMbid;
+        curr = riaaTree[curr.parentMbid];
+    }
+    return {"path": nodes};
 };
 
-// Respond to a valid request
-var respondToValidReq = function (mbid, res) {
-    res.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    // WRITE CODE HERE
-};
+// Create HTTP server
+var server = http.createServer(function (req, res) {
+    var mbid = urlParse(req.url, true).query.mbid,
+        httpCode = 400,
+        body = {"error": "MusicBrainz ID parameter (mbid) not specified"};
+    if (mbid !== undefined) {
+        httpCode = 200;
+        body = generateBody(mbid);
+    }
+    res.writeHead(httpCode, {"Content-Type": "application/json"});
+    res.end(JSON.stringify(body), "ascii");
+});
 
 // Start the HTTP server
-var startServer = function () {
-    http.createServer(function (req, res) {
-        var mbid = urlParse(req.url, true).query.mbid;
-        if (mbid) {
-            respondToValidReq(mbid, res);
-        } else {
-            respondToInvalidReq(res);
-        }
-    }).listen(httpPort, function () {
-        console.log("HTTP server listening on port " + httpPort);
-    });
-};
-
-// Start server after database is ready
-db.init(function () {
-    startServer();
+server.listen(httpPort, function () {
+    console.log("HTTP server listening on port " + httpPort);
 });
